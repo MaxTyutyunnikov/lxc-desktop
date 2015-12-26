c7c3b9508ef1f1915e2c767ec09cb231476e07b5
Max Tyutyunnikov <max.tyutyunnikov@gmail.com>
2015-12-26 11:46:09 +0300

    Work on my notebook

---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/ustuehler/lxc-desktop && cd lxc-desktop
+sudo apt-get install ubuntu-dev-tools debhelper
+dpkg-buildpackage -uc -us
+sudo dpkg -i ../lxc-desktop_*_all.deb
+sudo apt-get -f install
+
+sudo lxc-create -n container -t ubuntu-desktop
+#sudo lxc-start -n container -d
