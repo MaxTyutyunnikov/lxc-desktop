936742519612c1e718a00af098674a12dea3e756
Max Tyutyunnikov <max.tyutyunnikov@gmail.com>
2015-12-29 19:04:04 +0300

    test

--- root/scripts/01_install.sh
+++ root/scripts/01_install.sh
@@ -2,8 +2,10 @@
 
 git clone https://github.com/ustuehler/lxc-desktop
 cd lxc-desktop
+git pull --all
 sudo apt-get install ubuntu-dev-tools debhelper
 dpkg-buildpackage -uc -us
+sudo dpkg -r lxc-desktop
 sudo dpkg -i ../lxc-desktop_*_all.deb
 sudo apt-get -f install
 
