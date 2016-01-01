97030359533908a640cccb85f9c47f33577c06f0
Max Tyutyunnikov <max.tyutyunnikov@gmail.com>
2016-01-01 14:04:41 +0300

    test

--- push.sh
+++ push.sh
@@ -5,6 +5,6 @@ git commit -a -m test
 git push --all
 
 ssh book mkdir /root/lxc-desktop
-ssh book git clone "git@github.com:MaxTyutyunnikov/lxc-desktop.git" /root/lxc-desktop
+ssh book git clone "git@github.com:MaxTyutyunnikov/lxc-desktop.git" /root
 ssh book git pull --all /root/lxc-desktop
 #ssh book /root/lxc-desktop/root/scripts/01_install.sh
