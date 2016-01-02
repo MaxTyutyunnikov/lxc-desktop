092334baa22e20196db94bcd324d5464d2a6b26a
Max Tyutyunnikov <max.tyutyunnikov@gmail.com>
2016-01-02 10:25:00 +0300

    test

--- push.sh
+++ push.sh
@@ -15,3 +15,5 @@ ssh book /root/lxc-desktop/root/scripts/01_install.sh
 echo === 05 =============================================================
 ##ssh book /root/lxc-desktop/root/scripts/02_create.sh
 echo === 06 =============================================================
+ssh book /root/lxc-desktop/root/scripts/03_restart.sh
+echo === 07 =============================================================
