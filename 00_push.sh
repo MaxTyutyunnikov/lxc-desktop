926f6bd3835e1ff28ce46b38460483f8076ee322
Max Tyutyunnikov <max.tyutyunnikov@gmail.com>
2016-01-03 23:38:36 +0300

    test

--- 00_push.sh
+++ 00_push.sh
@@ -13,7 +13,7 @@ ssh book cd /root/lxc-desktop && git pull --all
 echo === 04 =============================================================
 ssh book /root/lxc-desktop/root/scripts/01_install.sh
 echo === 05 =============================================================
-##ssh book /root/lxc-desktop/root/scripts/02_create.sh
+ssh book /root/lxc-desktop/root/scripts/02_create.sh
 echo === 06 =============================================================
 ssh book /root/lxc-desktop/root/scripts/03_restart.sh
 echo === 07 =============================================================
--- root/scripts/02_create.sh
+++ root/scripts/02_create.sh
@@ -10,6 +10,6 @@ sudo lxc-destroy -n container
 sudo lxc-destroy -n c1
 sudo lxc-destroy -n c2
 
-sudo lxc-create -n container -t ubuntu-desktop -l debug -o debug.log -- --debug --packages="fglrx,gnome-terminal,mc,aptitude,dkms" || exit 1
+sudo lxc-create -n container -t ubuntu-desktop -l debug -o debug.log -- --debug --packages="gnome-terminal,mc,aptitude,dkms" || exit 1
 sudo lxc-clone container c1
 sudo lxc-clone container c2
